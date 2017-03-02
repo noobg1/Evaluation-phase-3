29dac9ef4ec32d5e2d5973ad9196269679da2ed1
noobg1 <jeevu.art@gmail.com>
2017-03-02 10:54:07 +0530

    formatted releasedate using moment.js

--- movieApp/src/Movie.jsx
+++ movieApp/src/Movie.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
+import moment from 'moment'
 
 class Movie extends Component {
     render() {
-      const movieDetails = this.props.movieDetails
-      let movieActors = movieDetails.actors.map((actor) => <li key={actor}>{actor}</li>)
-      return (
+        const movieDetails = this.props.movieDetails
+        let movieActors = movieDetails.actors.map((actor) => <li key={actor}>{actor}</li>)
+        return (
       <div className="">
         <h2>{movieDetails.name} </h2>
         <p>{movieDetails.description}</p>
         <ul>
           {movieActors}
+          Release Date: {moment(movieDetails.releaseDate).format('Do MMMM YYYY')}
         </ul>
       </div>
     )
-  }
+    }
 }
 
 export default Movie
